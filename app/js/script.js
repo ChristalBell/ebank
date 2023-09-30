9670864eb53ffa60cf78cb091a3099723f7d6c1e
Christal Bell <christalvictoriabell@gmail.com>
2023-09-29 21:47:30 -0700

    animated mobile menu

--- app/js/script.js
+++ app/js/script.js
@@ -1,17 +1,22 @@
 const btnHamburger = document.querySelector("#btnHamburger");
 const header = document.querySelector(".header");
 const overlay = document.querySelector(".overlay");
+const fadeElems = document.querySelectorAll(".has-fade");
 
 btnHamburger.addEventListener("click", function () {
   console.log("open hamburger");
 
   if (header.classList.contains("open")) {
     header.classList.remove("open");
-    overlay.classList.remove("fade-in");
-    overlay.classList.add("fade-out");
+    fadeElems.forEach(function (element) {
+      element.classList.remove("fade-in");
+      element.classList.add("fade-out");
+    });
   } else {
     header.classList.add("open");
-    overlay.classList.add("fade-in");
-    overlay.classList.remove("fade-out");
+    fadeElems.forEach(function (element) {
+      element.classList.add("fade-in");
+      element.classList.remove("fade-out");
+    });
   }
 });
