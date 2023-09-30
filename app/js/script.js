f3f67910b61493bc58490306e521faa4aefd671b
Christal Bell <christalvictoriabell@gmail.com>
2023-09-29 22:03:24 -0700

    noscroll added

--- app/js/script.js
+++ app/js/script.js
@@ -1,4 +1,5 @@
 const btnHamburger = document.querySelector("#btnHamburger");
+const body = document.querySelector("body");
 const header = document.querySelector(".header");
 const overlay = document.querySelector(".overlay");
 const fadeElems = document.querySelectorAll(".has-fade");
@@ -7,12 +8,14 @@ btnHamburger.addEventListener("click", function () {
   console.log("open hamburger");
 
   if (header.classList.contains("open")) {
+    body.classList.remove("noscroll");
     header.classList.remove("open");
     fadeElems.forEach(function (element) {
       element.classList.remove("fade-in");
       element.classList.add("fade-out");
     });
   } else {
+    body.classList.add("noscroll");
     header.classList.add("open");
     fadeElems.forEach(function (element) {
       element.classList.add("fade-in");
