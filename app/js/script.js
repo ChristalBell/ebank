89f991f3164cc5d63c4ca9f92219a7b7e11e03e1
Christal Bell <christalvictoriabell@gmail.com>
2023-10-06 11:09:44 -0700

    trying to add drop down menu over hero image

--- app/js/script.js
+++ app/js/script.js
@@ -4,19 +4,26 @@ const header = document.querySelector(".header");
 const overlay = document.querySelector(".overlay");
 const fadeElems = document.querySelectorAll(".has-fade");
 
+// close hamburger menu
 btnHamburger.addEventListener("click", function () {
   console.log("open hamburger");
 
   if (header.classList.contains("open")) {
     body.classList.remove("noscroll");
     header.classList.remove("open");
+    // document.getElementById("hero__image").style.transition =
+    //   "all 300ms ease-in-out";
+
     fadeElems.forEach(function (element) {
       element.classList.remove("fade-in");
       element.classList.add("fade-out");
     });
   } else {
+    // open
     body.classList.add("noscroll");
     header.classList.add("open");
+    // document.getElementById("hero__image").style.visibility = "hidden";
+
     fadeElems.forEach(function (element) {
       element.classList.add("fade-in");
       element.classList.remove("fade-out");
